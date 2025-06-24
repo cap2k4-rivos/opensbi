1e216ad9defae61e3a876bb0f42ed4643d50e5fb
Akshay Behl <cap2k4@rivosinc.com>
2025-06-24 09:51:14 +0530

    lib: utils: mpxy: Added Management Mode message handler
    In efforts to split the send message handling for
    Management Mode and Request Forward, this handler
    will handle all the calls from Management Mode.
    
    Signed-off-by: Dhaval Sharma <dhaval@rivosinc.com>
    Signed-off-by: Akshay Behl <cap2k4@rivosinc.com>

---
+++ lib/utils/mpxy/mpxy_mm_handler.c
@@ -0,0 +1,52 @@
+#include <sbi/sbi_error.h>
+#include <sbi/sbi_heap.h>
+#include <sbi/sbi_mpxy.h>
+#include <libfdt.h>
+#include <sbi_utils/mpxy/fdt_mpxy.h>
+#include <sbi/sbi_domain.h>
+#include <sbi/sbi_console.h>
+#include <sbi_utils/mpxy/mm.h>
+
+/** 
+ * TODO: Instead of hardcoding these values
+ * these should be fetched from the device tree
+ * instead.
+ */
+#define MM_SHMEM_ADDR_LOW 0xFFE00000
+#define MM_SHMEM_SIZE 0x200000
+
+int sbi_mpxy_mm_message_handler(struct mpxy_channel_info *channel_info,
+				  u32 msg_id, void *msgbuf, u32 msg_len,
+			    void *respbuf, u32 resp_max_len,
+			    unsigned long *ack_len, 
+                struct sbi_domain *switch_to_domain)
+{
+	update_channel_data_len(channel_info->channel.channel_id, msg_len);
+
+	if (RISCV_MSG_ID_SMM_VERSION == msg_id) {
+		struct mm_get_attributes attr;
+		memset(&attr, 0, sizeof(struct mm_get_attributes));
+		attr.status	 = 0;
+		attr.mm_version = SMM_VERSION_COMPILED;
+		attr.mm_shmem_addr_low = MM_SHMEM_ADDR_LOW;
+		attr.mm_shmem_size     = MM_SHMEM_SIZE;
+		sbi_memcpy((void *)respbuf, &attr,
+			   sizeof(struct mm_get_attributes));
+		if (ack_len)
+			*ack_len = sizeof(struct mm_get_attributes);
+	} else if (RISCV_MSG_ID_SMM_EVENT_COMPLETE == msg_id) {
+		sbi_domain_context_exit();
+
+	} else if (RISCV_MSG_ID_SMM_COMMUNICATE == msg_id) {
+        update_channel_data_len(channel_info->server_channel_id, 0);
+		sbi_mpxy_copy_context(channel_info->channel_domain, channel_info->channel.channel_id,
+                            switch_to_domain, channel_info->server_channel_id, 0);
+		sbi_domain_context_enter(switch_to_domain);
+        if(get_channel_data_len(channel_info->channel.channel_id) == 0)
+            return SBI_EFAIL;
+	} else {
+		return SBI_EFAIL;
+	}
+
+	return SBI_OK;
+}
