5dd131fbf5c779d8c4d7524ee71f16a03f59d960
Akshay Behl <cap2k4@rivosinc.com>
2025-08-06 16:49:05 +0530

    lib: utils: mpxy: Add more prints to help understand control flow
    Signed-off-by: Akshay Behl <cap2k4@rivosinc.com>

--- include/sbi_utils/mpxy/mm.h
+++ include/sbi_utils/mpxy/mm.h
@@ -9,9 +9,9 @@
  * refer to: https://uefi.org/specs/PI/1.8/V4_Overview.html for more details.
  */
 
-#define RISCV_MSG_ID_SMM_VERSION		0x1
-#define RISCV_MSG_ID_SMM_COMMUNICATE	0x4
-#define RISCV_MSG_ID_SMM_EVENT_COMPLETE 0x3
+#define RISCV_MSG_ID_SMM_ENABLE_NOTIFICATION  0x1
+#define RISCV_MSG_ID_SMM_GET_ATTRIBUTES       0x2
+#define RISCV_MSG_ID_SMM_COMMUNICATE          0x3
 
 #define SMM_VERSION_MAJOR        1
 #define SMM_VERSION_MAJOR_SHIFT  16
--- lib/utils/mpxy/mpxy_mm_handler.c
+++ lib/utils/mpxy/mpxy_mm_handler.c
@@ -22,8 +22,9 @@ int sbi_mpxy_mm_message_handler(struct mpxy_channel_info *channel_info,
                 struct sbi_domain *switch_to_domain)
 {
 	update_channel_data_len(channel_info->channel.channel_id, msg_len);
-
-	if (RISCV_MSG_ID_SMM_VERSION == msg_id) {
+	sbi_printf("[OpenSBI MPXY] Management Mode Service Handler: channel_id: %d, message_id: %d, message_type: ", channel_info->channel.channel_id, msg_id);
+	if (RISCV_MSG_ID_SMM_GET_ATTRIBUTES == msg_id) {
+		sbi_printf("MM_GET_ATTRIBUTES\n");
 		struct mm_get_attributes attr;
 		memset(&attr, 0, sizeof(struct mm_get_attributes));
 		attr.status	 = 0;
@@ -34,10 +35,8 @@ int sbi_mpxy_mm_message_handler(struct mpxy_channel_info *channel_info,
 			   sizeof(struct mm_get_attributes));
 		if (ack_len)
 			*ack_len = sizeof(struct mm_get_attributes);
-	} else if (RISCV_MSG_ID_SMM_EVENT_COMPLETE == msg_id) {
-		sbi_domain_context_exit();
-
 	} else if (RISCV_MSG_ID_SMM_COMMUNICATE == msg_id) {
+		sbi_printf("MM_COMMUNICATE\n");
         update_channel_data_len(channel_info->server_channel_id, 0);
 		sbi_mpxy_copy_context(channel_info->channel_domain, channel_info->channel.channel_id,
                             switch_to_domain, channel_info->server_channel_id, 0);
